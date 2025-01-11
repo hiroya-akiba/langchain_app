427e0fe0bb3aebb757725a2f26403432d2f97314
hiroya-akiba <hiroya0504@gmail.com>
2025-01-11 15:34:20 +0900

    2025/01/11 Promptsの使い方

--- basic_langchain/prompts.py
+++ basic_langchain/prompts.py
@@ -1,23 +1,30 @@
 # 2025/01/11時点でのソース
 import os
+from langchain.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate
+)
+
 from langchain.chat_models import openai
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
 
 # APIキーをセット
 filepath = "../auth_info/open_ai_20250110.key"
 with open(filepath, "r", encoding="utf-8") as f:
     os.environ["OPENAI_API_KEY"] = f.readline()
 
-# ストリ－ミングで応答を得る方法
-chat = openai.ChatOpenAI(
-    model_name="gpt-3.5-turbo", 
-    temperature=0, 
-    streaming=True,
-    callbacks=[StreamingStdOutCallbackHandler()]
-    )
-messages = [
-    SystemMessage(content="You are a programming helpful assistant."),
-    HumanMessage(content="こんにちは。私はJavaとPythonを書くプログラマです。"),
-    ]
+# ChatPromptTemplateの使い方
+## Chat Completions APIの形式に対応したメッセージを作ると、以下のようになる。
+## プロンプトの指定部分に差し込んで作成される。
+chat_prompt = ChatPromptTemplate.from_messages([
+    SystemMessagePromptTemplate.from_template("あなたは{lang}言語のプロフェッショナルです。"),
+    HumanMessagePromptTemplate.from_template("以下のアプリケーションを作成する際に、どんなパッケージを揃えたらよいか教えてください。\n\nアプリケーション : {app}")
+])
+messages = chat_prompt.format_prompt(lang="Java", app="お気に入りメール送信アプリケーション").to_messages()
+
+# Chatモデル作成
+chat = openai.ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 result = chat(messages)
+print(result)
